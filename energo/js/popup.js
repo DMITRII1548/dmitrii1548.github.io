11b54c8f7fefc88ae24ea2a62b5fdd2b2ba4b39c
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2024-03-26 09:33:18 +0600

    Add files via upload

---
+++ energo/js/popup.js
@@ -0,0 +1,16 @@
+document.getElementById('popupClose').onclick = () => {
+    document.getElementById('popup').classList.toggle('none')
+}
+
+document.getElementById('popupOpen').onclick = () => {
+    document.getElementById('popup').classList.toggle('none')
+}
+
+document.getElementById('popupOpen1').onclick = () => {
+    document.getElementById('popup').classList.toggle('none')
+}
+
+document.getElementById('popupOpen2').onclick = () => {
+    document.getElementById('popup').classList.toggle('none')
+}
+
