76c9755a117fd77703df404f4a480d4f01a2df11
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2024-03-26 09:32:10 +0600

    Delete energo directory

--- energo/js/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('popupClose').onclick = () => {
-    document.getElementById('popup').classList.toggle('none')
-}
-
-document.getElementById('popupOpen').onclick = () => {
-    document.getElementById('popup').classList.toggle('none')
-}
