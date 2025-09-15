eaf8ebdd8cb55f328c8bd36745e84f08ff7cbf30
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-09-15 17:30:46 +0600

    Update avto website

--- avto/js/menu.js
+++
@@ -1,8 +0,0 @@
-{
-    const menu = document.getElementById('nav-menu')
-    const btnToggle = document.getElementById('nav-toggle')
-
-    btnToggle.addEventListener('click', () => {
-        menu.classList.toggle('d-none')
-    })
-}
